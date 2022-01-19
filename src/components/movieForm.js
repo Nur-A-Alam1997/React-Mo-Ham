1c4dc13a1e4cf66676a04c7f6a5cc6133e7cfb5f
nur <nuraalamp@gmail.com>
2022-01-19 20:05:20 +0600

    moviForm added

--- src/components/movieForm.js
+++ src/components/movieForm.js
@@ -1,34 +1,49 @@
 import React from "react"
 
-class MovieForm extends React.Component{
+class MovieForm extends React.Component {
 
-    constructor(){
+    constructor() {
         super()
+        this.state = {
+            account: {
+                email: 'email@gmail.com',
+                password: '*123~'
+            }
+        }
     }
-    formHandler = e =>{
+    formSubmit = (e) => {
         console.log("yead")
         e.preventDefault()
     }
-    render(){
+
+    formChange = (e) => {
+        const account = {...this.state.account}
+        account[e.currentTarget.name] = e.currentTarget.value
+        // account.password = e.currentTarget.value
+        this.setState({account})
+    }
+
+    render() {
+        const { email, password } = this.state.account;
         return <>
-        <form onSubmit={this.formHandler} >
-            <div className="form-group">
-                <label or="exampleInputEmail1">Email address</label>
-                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
-                    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-            </div>
-            <div className="form-group">
-                <label or="exampleInputPassword1">Password</label>
-                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
-            </div>
-            <div className="form-group form-check">
-                <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+            <form onSubmit={this.formSubmit} >
+                <div className="form-group">
+                    <label htmlFor="email">Email address </label>
+                    <input onChange={this.formChange} name="email" type="email" value={email} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"  />
+
+                </div>
+                <div className="form-group">
+                    <label or="exampleInputPassword1">Password </label>
+                    <input  onChange={this.formChange} name="password" type="password" value={password}   className="form-control" id="exampleInputPassword1"  />
+                </div>
+                <div className="form-group form-check">
+                    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
                     <label className="form-check-label" or="exampleCheck1">Check me out</label>
-            </div>
-            <button  className="btn btn-primary">Submit</button>
-        </form>
-    </>
-}
+                </div>
+                <button className="btn btn-primary">Submit</button>
+            </form>
+        </>
+    }
 }
 
 export default MovieForm
